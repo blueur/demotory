baf8ce4c1c4fca795c5942e9bd0c882dd67b33c6
David Tang <dev@blueur.com>
2023-12-15 01:42:45 +0100

    feat: add entries

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # demotory
+
+```bash
+# http://localhost:8080
+mvn spring-boot:run
+
+# http://localhost:8081
+mvn spring-boot:run -Dspring-boot.run.arguments=--server.port=8081
+```
---
+++ src/main/java/com/blueur/demotory/RootController.java
@@ -0,0 +1,47 @@
+package com.blueur.demotory;
+
+import java.util.Map;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.map.IMap;
+
+@Controller
+public class RootController {
+  @Autowired
+  private HazelcastInstance hazelcastInstance;
+
+  private IMap<String, String> entriesMap() {
+    return hazelcastInstance.getMap("entries");
+  }
+
+  @GetMapping
+  public String index(Model model) {
+    final Map<String, String> entries = entriesMap().getAll(entriesMap().keySet());
+    model.addAttribute("entries", entries);
+    model.addAttribute("entry",
+        new Entry(RandomStringUtils.randomAlphanumeric(2), RandomStringUtils.randomAlphanumeric(4)));
+    return "index";
+  }
+
+  @PostMapping
+  public String put(@ModelAttribute Entry entry) {
+    entriesMap().put(entry.getKey(), entry.getValue());
+    return "redirect:.";
+  }
+
+  @DeleteMapping("/{key}")
+  public String delete(@PathVariable String key) {
+    entriesMap().delete(key);
+    return "redirect:.";
+  }
+}
